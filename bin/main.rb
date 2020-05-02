d95c19bf17c705ecd96dfe9328ac301c03aab4b3
frankopkusianwar <okirorfrank3@gmail.com>
2020-05-02 12:21:35 +0300

    mock the required flow of the proogram to lint files

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,7 @@
 #!/usr/bin/env ruby
+puts '1. check if path is provided'
+puts '2. if a path is found or provided'
+puts'3. check if its a file or directory'
+puts '4. lint the files whether directory or file'
+puts '5. otherwise if there is no path check lint errors for all files in current directory that match a particular type'
+puts '6. error type in file #filename on line #line number. #error msg'
